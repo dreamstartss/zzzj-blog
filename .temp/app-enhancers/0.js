b49eac7e3d0d8af645a0ef0871d0655aadf73cb4
dreamstarss <1551483343@qq.com>
2022-06-06 21:56:19 +0800

    随便改改

---
+++ .temp/app-enhancers/0.js
@@ -0,0 +1,2 @@
+import "D:\\study\\vlog\\zzzj-blog\\node_modules\\@vuepress\\core\\lib\\node\\internal-plugins\\style\\client.js"
+export default {}
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,15 @@
 module.exports = {
+  title: '梦想远方',
+  description: '诗和远方',
   themeConfig: {
     logo: '/assets/img/logo.png',
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Guide', link: '/about' },
-      { text: 'External', link: 'https://google.com' },
+      { text: 'JS', link: '/js/JS' },
+      { text: 'CSS', link: '/css/css' },
+      { text: 'TS', link: '/ts/TS' },
     ],
+    sidebar: 'auto',
+    lastUpdated: 'Last Updated',
   },
 }
